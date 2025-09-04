6c214e5914cf2249e6ecb67dad72367c9bd34192
A-Yarrow <yarrowmadrona@gmail.com>
2025-09-04 10:02:14 -0700

    Removed stdout redirection. Was breaking getopt

--- install_bindcraft.sh
+++ install_bindcraft.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Build with: docker build --progress=plain -t bindcraft:test .
 ################## BindCraft installation script optimized for RunPod 12.4 base image: FROM runpod/pytorch:2.4.0-py3.11-cuda12.4.1-devel-ubuntu22.04
 ################## tested on 2025-09-04
@@ -15,12 +14,6 @@ LONGOPTIONS=pkg_manager:,cuda:
 # Parse the command-line options
 PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTIONS --name "$0" -- "$@")
 eval set -- "$PARSED"
-
-set -x
-export BASH_XTRACEFD=1 # redirect xtrace to stdout
-exec 1>&2 # redirect stdout to stderr
-export PYTHONUNBUFFERED=1 # make python output unbuffered
-export PIP_PROGRESS_BAR=off 
 
 # Process the command-line options
 while true; do
@@ -154,5 +147,3 @@ echo -e "Successfully finished BindCraft installation!\n"
 echo -e "Activate environment using command: \"$pkg_manager activate BindCraft\""
 echo -e "\n"
 echo -e "Installation took $(($t / 3600)) hours, $((($t / 60) % 60)) minutes and $(($t % 60)) seconds."
-
-set +x # stop echoing commands
