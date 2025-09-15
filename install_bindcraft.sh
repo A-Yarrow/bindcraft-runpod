e4c2b6e587ed2632992536d51e4ec1a9112e6ff2
A-Yarrow <yarrowmadrona@gmail.com>
2025-09-15 13:49:37 -0700

    pinned jupyterlab and ipywidgets

--- install_bindcraft.sh
+++ install_bindcraft.sh
@@ -36,9 +36,10 @@ echo "BindCraft environment activated at ${CONDA_BASE}/envs/BindCraft"
 echo "Installing conda packages"
 $pkg_manager install \
   pip pandas matplotlib 'numpy<2.0.0' biopython scipy pdbfixer seaborn libgfortran5 tqdm \
-  jupyter jupyterlab ffmpeg pyrosetta fsspec py3dmol \
+  jupyter=1.0 jupyterlab=3.6.3 ffmpeg pyrosetta fsspec py3dmol \
   chex dm-haiku 'flax<0.10.0' dm-tree joblib ml-collections immutabledict optax \
   psutil copyparty \
+  ipywidgets=7.7.2 \
   -c conda-forge --channel https://conda.graylab.jhu.edu -y \
 || { echo "Error: Failed to install conda packages"; exit 1; }
 
